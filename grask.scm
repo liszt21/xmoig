38d37058aa03ef09f0ebcc8c8be69e35f745b056
liszt21 <liszt21@qq.com>
2023-07-18 22:39:41 +0800

    feat: update xmonad config

---
+++ grask.scm
@@ -0,0 +1,44 @@
+(define-module (grask))
+ #:use-module (gnu packages wm)
+ #:use-module (gnu packages haskell-xyz)
+ #:use-module (gnu packages xorg)
+ #:use-module (guix gexp)
+ #:use-module (guix git-download)
+ #:use-module (guix packages)
+ #:use-module (guix utils)
+ #:use-module (ice-9 popen)
+ #:use-module (ice-9 rdelim)
+ #:use-module (ice-9 regex))
+
+(define %local
+  (local-file (dirname (current-filename))
+              #:recursive? #t
+              #:select?
+              (lambda (f _)
+                (not
+                 (or (string-match "\\.ghc\\.environment" f)
+                     (string-match "dist" f)
+                     (string-match "dist-newstyle" f))))))
+
+(display %local)
+
+(define-public grask-xmonad
+  (package
+    (inherit xmonad)
+    (name "grask-xmonad")
+    (version "0.0.0")
+    (source %local)
+    (inputs
+     `(("libxpm" ,libxpm)
+       ("xmobar" ,xmobar)
+       ("xmonad" ,xmonad)
+       ("ghc-random" ,ghc-random)
+       ("ghc-uuid" ,ghc-uuid)
+       ("ghc-xmonad-contrib" ,ghc-xmonad-contrib)))
+    (arguments
+     `(#:phases
+       (modify-phases %standard-phases
+         (add-after 'install 'make-static
+           (lambda* (#:key outputs #:allow-other-keys)
+             (mkdir-p (assoc-ref outputs "static"))))
+         (delete 'install-license-files))))))
